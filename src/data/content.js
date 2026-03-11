7dc6ce6b9ba32e037bf0c0ddf2d0d1d782b5cdc3
Neville Oronni <nevooronni@gmail.com>
2026-03-11 09:43:11 +0300

    Update content and add new image

--- src/data/content.js
+++ src/data/content.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "Babbel",
     shortDescription: "Millions of people learn languages with Babbel — the app built by language learning experts. Our short, interactive lessons rethink old-school language education to get you speaking a new language with confidence.",
     fullDescription: "Millions of people learn languages with Babbel — the app built by language learning experts. Our short, interactive lessons rethink old-school language education to get you speaking a new language with confidence. integrated with OpenAI API for AI based learning. LLM modles used GTP5, GPT4 omni",
-    techStack: ["React Native", "Expo", "EAS", "ios", "android", "GraphQL", "LLM", "OpenAI API"],
+    techStack: ["React Native", "Expo", "EAS",  "GraphQL", "LLM", "OpenAI API", "Couchbase", "Express"],
     image: "babel.png",
     repoLink: "#",
     liveLink: "https://play.google.com/store/apps/details?id=com.babbel.mobile.android.en&referrer=adjust_reftag%3Dcy1Xa8gGTF8JF%26utm_source%3Dbackup_mobiletracking_%252Fmobile"
@@ -14,7 +14,7 @@ export const projects = [
     title: "StagePass",
     shortDescription: "An events ticketing platform backend system for event organizers, event attendees and event managers",
     fullDescription: "An events ticketing platform that allows users to purchase tickets for events and also for event organizers to sell tickets for their events and collect payments.",
-    techStack: ["Next", "Tanstack", "cloudflare", "Vercel", "shadcn/ui"],
+    techStack: ["REST API", "Lemonade API","NextJS", "Tanstack", "cloudflare", "Vercel", "shadcn/ui"],
     image: "lemonade5.png",
     repoLink: "#",
     liveLink: "https://stagepass.vip/"
@@ -24,7 +24,7 @@ export const projects = [
     title: "StagePass Events Backend",
     shortDescription: "An events ticketing platform backend system for event organizers, event attendees and event managers",
     fullDescription: "backend system for stagepass An events ticketing platform that allows users to purchase tickets for events and also for event organizers to sell tickets for their events and collect payments.",
-    techStack: ["Nest.js", "PostgreSQL", "redis", "jenkins", "Digital Ocean", "Docker"],
+    techStack: ["Nest.js", "PostgreSQL", "redis", "Kafka", "jenkins", "Digital Ocean", "Docker"],
     image: "lemonade1.png",
     repoLink: "#",
     liveLink: "https://stagepass.vip/docs/#"
@@ -34,7 +34,7 @@ export const projects = [
     title: "Twiga DMS Mobile",
     shortDescription: "A data management system for Twiga Foods. Incorporates features for adding new users, assigning roles, harvest management, sourcing management, sales management, and processing management.",
     fullDescription: "A data management system for Twiga Foods. Incorporates features for adding new users, assigning roles, harvest management, sourcing management, sales management, and processing management.",
-    techStack: ["React Native", "Expo", "EAS", "ios", "android", "GraphQL"],
+    techStack: ["React Native", "Expo", "EAS", "ios", "android", "GraphQL", "MPESA API"],
     role: "Software Developer",
     image: "twiga2.jpeg",
     repoLink: "#",
@@ -45,7 +45,7 @@ export const projects = [
     title: "Twiga Soko Yetu Mobile",
     shortDescription: "A mobile app for twiga called Soko Yetu for for deliveries across multiple town and cities in Kneya",
     fullDescription: "Soko Yetu Android App brings Twiga Foods' self ordering platform closer to you. With only a tap of the app you can access features such as placing orders, viewing deliveries made to you and making your payments to Twiga among other features",
-    techStack: ["React Native", "Expo", "EAS", "ios", "android"],
+    techStack: ["React Native", "Expo", "EAS", "ios", "android", "GraphQL", "MPESA API"],
     role: "Software Developer",
     image: "twiga4.png",
     repoLink: "#",
@@ -56,9 +56,9 @@ export const projects = [
     title: "Lemonade Mobile",
     shortDescription: "A mobile app for lemonade payments operating across multiple Africa countries enabling both business and personal payments",
     fullDescription: "A comprehensive mobile app for lemonade payments enables collections via M-PESA, Airtel Money, and cards, along with instant, bulk transfers and multi-currency, secure transactions to reduce operational friction. ",
-    techStack: ["React Native", "Expo", "EAS", "ios", "android"],
+    techStack: ["React Native", "Expo", "EAS", "ios", "android", "VoveID", "Lemonade API",],
     role: "Software Developer",
-    image: "lemonade3.png",
+    image: "lemon_10.png",
     repoLink: "#",
     liveLink: "https://play.google.com/store/apps/details?id=com.nyasi01.lemonade&hl=en"
   },
@@ -78,7 +78,7 @@ export const projects = [
     title: "Lemonade Payments system",
     shortDescription: "Backend system for Lemonade Payments operating across multiple Africa countries",
     fullDescription: "A comprehensive business payments system for lemonade payments enables collections via M-PESA, Airtel Money, and cards, along with instant, bulk transfers and multi-currency, secure transactions to reduce operational friction and cross border payments.",
-    techStack: ["Node.js", "Express", "PostgreSQL", "Digital Ocean", "Docker"],
+    techStack: ["Node.js", "NestJS", "PostgreSQL", "Redis", "RabbitMQ", "Digital Ocean", "Jenkins", , "Docker"],
     role: "Software Developer",
     image: "lemonade1.png",
     repoLink: "#",
@@ -89,7 +89,7 @@ export const projects = [
     title: "Workpay Africa Mobile",
     shortDescription: "HR payroll software for Workpay Africa. Enables user to process payroll, manage time & attendance & NHIF, NSSF & PAYE calculators.",
     fullDescription: "A comprehensive HR and payroll solution mobile app designed for African businesses. Includes features for automated payroll processing, statutory deductions (NHIF, NSSF, PAYE), employee management, and time tracking.",
-    techStack: ["Node.js", "MySQL", "ReactJS", "Digital Ocean"],
+    techStack: ["React Native", "Expo", "EAS", "ios", "android", "SmileID", "MPESA API",],
     role: "Software Developer",
     image: "workpay1.png",
     repoLink: "#",
@@ -100,7 +100,7 @@ export const projects = [
     title: "Workpay Africa",
     shortDescription: "HR payroll software for Workpay Africa. Enables user to process payroll, manage time & attendance & NHIF, NSSF & PAYE calculators.",
     fullDescription: "A comprehensive HR and payroll solution designed for African businesses. Includes features for automated payroll processing, statutory deductions (NHIF, NSSF, PAYE), employee management, and time tracking.",
-    techStack: ["Node.js", "MySQL", "ReactJS", "Digital Ocean"],
+    techStack: ["Node.js", "Express", "MySQL", "ReactJS", "Digital Ocean", "Jenkins", "Docker", "vite"],
     role: "Software Developer",
     image: "workpay_login.png",
     repoLink: "#",
@@ -111,21 +111,24 @@ export const projects = [
     title: "GALS Web App",
     shortDescription: "GALS (Gender Action Learning System) Web App. Supports the Gender Action Learning System methodology developed by variuos NGOs in Africa i.e IFAD, UNICEF, UNDP, UNAIDS. Features include data tracking, progress monitoring, and resource sharing for community empowerment.",
     fullDescription: "A web application supporting the Gender Action Learning System methodology. Features include data tracking, progress monitoring, and resource sharing for community empowerment.",
-    techStack: ["Bootstrap", "Node.js", "React", "Material Design", "GitLab", "Docker"],
+    techStack: ["Django", "NextJS", "Chakra", "GitLab", "Docker", "Digital Ocean", "Jenkins", "MySQL"],
     role: "Software Developer",
     image: "gals1.png",
-    repoLink: "https://github.com/nevooronni/Demo_2",
-    liveLink: "https://gals.co.ke"
+    // repoLink: "https://github.com/nevooronni/Demo_2",
+    repoLink: "#",
+    // liveLink: "https://gals.co.ke",
+    liveLink: "https://demo-2-nevooronni.vercel.app/"
   },
   {
     id: "gals_dashboard",
     title: "GALS Dashboard",
     shortDescription: "GALS Administrator Dashboard for GALS (Gender Action Learning System) Web App. Supports the Gender Action Learning System methodology developed by variuos NGOs in Africa i.e IFAD, UNICEF, UNDP, UNAIDS.",
     fullDescription: "The administrator dashboard for the GALS application. Allows users with admin privileges to manage user data, monitor core system functionalities, and generate reports.",
-    techStack: ["Bootstrap", "Node.js", "React", "Material Design", "GitLab", "Docker"],
+    techStack: ["Django", "NextJS", "Chakra", "GitLab", "Docker", "Digital Ocean", "Jenkins", "MySQL"],
     role: "Software Developer",
     image: "gals4.png",
-    repoLink: "https://github.com/nevooronni/GALS_DMS",
+    // repoLink: "https://github.com/nevooronni/GALS_DMS",
+    repoLink: "#",
     liveLink: "https://galsadmin.co.ke"
   },
   {
@@ -133,10 +136,11 @@ export const projects = [
     title: "Telkom Dashboard",
     shortDescription: "Telkom Kenya's premiere mobile network service provider dashboard that provides integrated solutions to individuals, Small and Medium-sized Enterprises (SMEs), Government and large corporates in Kenya.",
     fullDescription: "A centralized dashboard for managing Telkom Kenya's services. It is capable of administering all of the mobile networks operator core service features and is highly dynamic.",
-    techStack: ["SASS", "Node.js", "React", "Java", "Docker", "GitLab"],
+    techStack: ["Express", "Node.js", "Typescript", "MySQL", "Redis", "React", "Jenkins", "Docker"],
     role: "Software Developer",
     image: "telkom.png",
-    repoLink: "https://github.com/nevooronni/telkom",
+    // repoLink: "https://github.com/nevooronni/telkom",
+    repoLink: "#",
     liveLink: "#"
   },
   {
@@ -144,10 +148,11 @@ export const projects = [
     title: "KPA Portal",
     shortDescription: "Kenya Port Authority online portal. At the Kenya Ports Authority, we are the dynamic heart of trade and commerce for Kenya and the East and Central Africa, connecting the region to global markets.",
     fullDescription: "The official v2 Kenya Port Authority online portal web app. Main functionality includes payment of import duties and accessing other rich port-related features.",
-    techStack: ["Bootstrap", "Node.js", "React", "Material Design", "GitLab", "Docker"],
+    techStack: ["ExpressJS", "Node.js", "Typescript", "MySQL", "Redis", "React", "Jenkins", "Docker"],
     role: "Software Developer",
     image: "kpa1.png",
-    repoLink: "https://github.com/nevooronni/kpa",
+    // repoLink: "https://github.com/nevooronni/kpa",
+    repoLink: "#",
     liveLink: "https://kpatest.co.ke"
   },
   {
@@ -155,11 +160,12 @@ export const projects = [
     title: "SGR Web App",
     shortDescription: "The Standard Gauge Railway (SGR) is a flagship project by the Government of Kenya as a transport component aimed at delivering Vision 2030 making Kenya a middle income country by 2030",
     fullDescription: "The official v2 Standard Gauge Railway web app. It allows users to book currently available trains for the Madaraka Express and others to travel all across Kenya.",
-    techStack: ["Bootstrap", "Node.js", "React", "Material Design", "GitLab", "Docker"],
+    techStack: ["FastifyJS", "Typescript", "MySQL", "Redis", "Docker", "Redis", "React", "Material UI"],
     role: "Software Developer",
     image: "sgr1.png",
-    repoLink: "https://github.com/nevooronni/SGR_App",
-    liveLink: "https://sgrest.co.ke"
+    // repoLink: "https://github.com/nevooronni/SGR_App",
+    repoLink: "#",
+    liveLink: "https://metickets.krc.co.ke/"
   },
   {
     id: "gamer",
@@ -169,7 +175,8 @@ export const projects = [
     techStack: ["Bootstrap", "jQuery", "Django", "Python", "Heroku", "MPESA API"],
     role: "Software Developer",
     image: "topup1.png",
-    repoLink: "https://github.com/nevooronni/credit_topup",
+    // repoLink: "https://github.com/nevooronni/credit_topup",
+    repoLink: "#",
     liveLink: "https://pinlesstopup.co.ke"
   },
   {
@@ -177,10 +184,11 @@ export const projects = [
     title: "Job Portal",
     shortDescription: "Bluco consultants job portal web app built for employers and candidates. A comprehensive job portal that allows users to register either as an employer or candidate.",
     fullDescription: "A comprehensive job portal that allows users to register either as an employer or candidate. Employers can post jobs, while candidates can search and apply for positions.",
-    techStack: ["Bootstrap", "React-Bootstrap", "ReactJS", "Node.js", "Python", "Digital Ocean"],
+    techStack: ["Firebase","Bootstrap", "React-Bootstrap", "ReactJS", "Node.js", "Python", "Digital Ocean"],
     role: "Software Developer",
     image: "bluco.png",
-    repoLink: "https://github.com/nevooronni/Bluco-frontendT",
+    // repoLink: "https://github.com/nevooronni/Bluco-frontendT",
+    repoLink: "#",
     liveLink: "https://bluco.co.ke"
   },
   {
@@ -191,31 +199,34 @@ export const projects = [
     techStack: ["Bootstrap", "GitHub API", "Node.js", "Gulp", "Bower"],
     role: "Project Owner, Software Developer",
     image: "politico1.png",
-    repoLink: "https://github.com/nevooronni/Politico_web_app/tree/gh-pages",
-    liveLink: "https://nevooronni.github.io/Politico_web_app/UI/html/index.html"
+    // repoLink: "https://github.com/nevooronni/Politico_web_app/tree/gh-pages",
+    repoLink: "#",
+    // liveLink: "https://nevooronni.github.io/Politico_web_app/UI/html/index.html"
+    liveLink: "https://politico.co.ke"
   },
   {
     id: "unsplash",
     title: "Collab Studio",
     shortDescription: "Web application that allows users collaborate on various projects involving content creation e.g music,art,movies e.t.c",
     fullDescription: "This is a web application that allows users collaborate on various projects involving content creation e.g music,art,movies e.t.c they can upload and share their content and even be able to play it through the application.",
-    techStack: ["Python", "Django", "PostgresSQL", "Boostrap", "Mpesa API"],
+    techStack: ["Python", "Django", "PostgresSQL", "Boostrap", "Mpesa API", "Heroku"],
     role: "Software Developer",
     image: "collab_studio.png",
-    repoLink: "https://github.com/nevooronni/Bluco-Admin",
-    liveLink: "https://blucoadmin.ml"
+    // repoLink: "https://github.com/nevooronni/Bluco-Admin",
+    repoLink: "#",
+    liveLink: "https://collabstudio.ml"
   },
-  {
-    id: "anita",
-    title: "Tarmaco career platform",
-    shortDescription: "Career platform for Tarmacore ltd for job seekers to find jobs and connect them with employers offering career opportunities.",
-    fullDescription: "A career platform that allows users to register, create video profiles for potential employers, and pay for additional subscription services.",
-    techStack: ["Bootstrap", "React-Bootstrap", "ReactJS", "Flask", "MySQL", "AWS", "GitLab", "Docker"],
-    role: "Software Developer",
-    image: "tarmac_main2.png",
-    repoLink: "https://github.com/nevooronni/Tarmacnomore_Frontend",
-    liveLink: "https://tarmacnomore.co.ke/"
-  },
+  //   {
+  //   id: "instagram",
+  //   title: "Bluco Backend & API",
+  //   shortDescription: "Comprehensive API service for Bluco consultants an employer and employee job platfiorm.",
+  //   fullDescription: "A backend API service allowing consumers to perform CRUD operations on Jobs, Employer, Employee, CV, Profile, Auth, and various other resources.",
+  //   techStack: ["Flask", "PostgreSQL", "Digital Ocean", "Jenkins", "Redis"],
+  //   role: "Software Developer",
+  //   image: "bluco_backend.png",
+  //   repoLink: "https://github.com/nevooronni/myproject",
+  //   liveLink: "https://blucogolang.tk"
+  // },
   {
     id: "datamanagement",
     title: "Data Management System",
@@ -261,15 +272,16 @@ export const projects = [
   //   liveLink: "https://blucojobsmpesa.tk"
   // },
   {
-    id: "instagram",
-    title: "Bluco Backend & API",
-    shortDescription: "Comprehensive API service for Bluco consultants an employer and employee job platfiorm.",
-    fullDescription: "A backend API service allowing consumers to perform CRUD operations on Jobs, Employer, Employee, CV, Profile, Auth, and various other resources.",
-    techStack: ["Flask", "PostgreSQL", "Digital Ocean", "Jenkins", "Redis"],
+    id: "anita",
+    title: "Tarmaco career platform",
+    shortDescription: "Career platform for Tarmacore ltd for job seekers to find jobs and connect them with employers offering career opportunities.",
+    fullDescription: "A career platform that allows users to register, create video profiles for potential employers, and pay for additional subscription services.",
+    techStack: ["Bootstrap", "React-Bootstrap", "ReactJS", "Flask", "MySQL", "AWS", "GitLab", "Docker"],
     role: "Software Developer",
-    image: "bluco_backend.png",
-    repoLink: "https://github.com/nevooronni/myproject",
-    liveLink: "https://blucogolang.tk"
+    image: "tarmac_main2.png",
+    // repoLink: "https://github.com/nevooronni/Tarmacnomore_Frontend",
+    repoLink: "#",
+    liveLink: "https://tarmacnomore.co.ke/"
   },
 ];
 
